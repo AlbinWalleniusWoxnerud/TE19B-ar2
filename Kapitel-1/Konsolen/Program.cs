f27e0095a7963d7bfa730486e235ba70d6743513
AlbinWalleniusWoxnerud <albin.walleniuswoxnerud@elev.ga.ntig.se>
2020-09-09 14:56:14 +0200

    Lesson 09/09-2020
    Lesson

---
+++ Kapitel-1/Konsolen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Konsolen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bakgrundsfärg terminal
+            Console.BackgroundColor = ConsoleColor.Blue;
+
+            // Textfärg terminal
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.Clear();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Kapitel-2/Exempel-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exempel_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklarera variabel för namnet
+            string namn = "Albin";
+            string hobby1 = "game";
+
+            // Specificerat meddelande
+            //string mede = "HejHejLeverPastej";
+
+            // Läs in namn från console
+            Console.Write("What u heter? ");
+            namn = Console.ReadLine();
+
+            // Läs in hobby från console
+            Console.Write("What hobby u have? ");
+            hobby1 = Console.ReadLine();
+
+            // Skriv ut namn och hobby
+            Console.WriteLine("Hello " + namn + "! Hobby: " + hobby1 + ".");
+
+            // Skriv ut specificerat meddelande
+            //Console.WriteLine(mede);
+        }
+    }
+}

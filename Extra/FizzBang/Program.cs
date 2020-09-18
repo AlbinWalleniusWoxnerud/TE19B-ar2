37e0d06bfa4bf4d0bafe3d36543adab0714d412d
AlbinWalleniusWoxnerud <albin.walleniuswoxnerud@elev.ga.ntig.se>
2020-09-18 15:46:16 +0200

    Extra

---
+++ Extra/FizzBang/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace FizzBang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int v1;
+            int v2;
+
+            for (i = 1; i <= 1000; i++)
+            {
+                v1 = i % 3;
+                v2 = i % 5;
+
+                if (v1 == 0 && v2 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (v1 == 0)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (v2 == 0)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}

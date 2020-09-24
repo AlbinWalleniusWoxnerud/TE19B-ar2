6e40794a8779a91678208cefb4e007279abaf12b
AlbinWalleniusWoxnerud <albin.walleniuswoxnerud@elev.ga.ntig.se>
2020-09-24 12:05:26 +0200

    More

--- Extra/Mini-DnD/Program.cs
+++ Extra/Mini-DnD/Program.cs
@@ -6,29 +6,32 @@ namespace Mini_DnD
     {
         static void Main(string[] args)
         {
-            string w1;
+            string c1;
+            string c2;
+            double death = 0;
 
             Console.Write("You awake in a dark room. As you look around you see three pedistals, each with a different item. A bow, A sword and a Shield. You wonder which one to choose. \nBow \nSword \nShield \nChoose one: ");
-            w1 = Console.ReadLine();
+            c1 = Console.ReadLine();
 
-            if (w1 == "Bow")
+            string msg = $"You choose the {c1}";
+
+            if (c1 == "Bow" || c1 == "Sword" || c1 == "Shield")
             {
-                Console.WriteLine($"You choose the {w1}");
-            }
-
-            else if (w1 == "Sword")
-            {
-
-                Console.WriteLine($"You choose the {w1}");
-            }
-
-            else if (w1 == "Shield")
-            {
-                Console.WriteLine($"You choose the {w1}");
+                Console.WriteLine(msg);
             }
             else
             {
+                death = 1;
                 Console.WriteLine("You failed to choose and died as a consequence.");
+            }
+            if (death == 0)
+            {
+                Console.Write($"As the {c1} enters your hands the pedistals sink into the ground. With a loud rumble the four walls surrounding you collapse reveling four passages. \nNorth \nEast \nSouth \nWest \nChoose a passage: ");
+                c2 = Console.ReadLine();
+                if (c2 == "North" || c2 == "East" || c2 == "South" || c2 == "West")
+                {
+                    Console.WriteLine(c2);
+                }
             }
         }
     }

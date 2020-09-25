53ed57fe945dae195bc7f681cf043ddbfc5fc61b
AlbinWalleniusWoxnerud <albin.walleniuswoxnerud@elev.ga.ntig.se>
2020-09-25 12:42:46 +0200

    heck

---
+++ Project/Test-Folder/Test-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Test_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write();
+        }
+        static void Write()
+        {
+            for (int i = 0; i <= string.Length; i++)
+            {
+
+                Thread.Sleep(200);
+                string letter = string.Substring(i, 1);
+                Console.Write(letter);
+            }
+        }
+
+    }
+}

bf761122ca5e199ed9b02e04eda03dd75a9c956e
AlbinWalleniusWoxnerud <albin.walleniuswoxnerud@elev.ga.ntig.se>
2020-11-12 10:53:10 +0100

    Proj 21
    21 is done

--- Project/21/Program.cs
+++ Project/21/Program.cs
@@ -1,22 +1,38 @@
 ﻿using System;
+using System.Threading;
 
 namespace _21
 {
     class Program
     {
+        static public string Winner = "";
+
+        static void Write(string strin)
+        {
+            for (int i = 0; i <= strin.Length - 1; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Thread.Sleep(20);
+                Console.Write(strin.Substring(i, 1));
+            }
+            if (strin.Length <= 10) Thread.Sleep(500);
+            Console.WriteLine("");
+        }
+
         static void Main()
         {
             int c1;
 
-            Console.WriteLine("Welcome to 21");
-            Console.WriteLine("Choose an option: ");
-            Console.WriteLine("1. Spela 21:an");
-            Console.WriteLine("2. Visa senaste vinnaren");
-            Console.WriteLine("3. Spelets regler");
-            Console.WriteLine("4. Avsluta programmet");
+            Write("Welcome to 21");
+            Write("Choose an option: ");
+            Write("1. Spela 21:an");
+            Write("2. Visa senaste vinnaren");
+            Write("3. Spelets regler");
+            Write("4. Avsluta programmet");
+            Write("");
             bool bc1 = int.TryParse(Console.ReadLine(), out c1);
             while(!bc1 || c1 < 1 || c1 > 4) {
-                Console.WriteLine("Invalid choise, choose again: ");
+                Write("Invalid choise, choose again: ");
                 bc1 = int.TryParse(Console.ReadLine(), out c1);
             }
             switch (c1) {
@@ -25,9 +41,15 @@ namespace _21
                 break;
 
                 case 2:
+                Write($"Senaste vinnaren: {Winner}");
+                Write("");
+                Main();
                 break;
 
                 case 3:
+                Write("4789350923465934865+12968");
+                Write("");
+                Main();
                 break;
 
                 case 4:
@@ -51,7 +73,7 @@ namespace _21
             while (p) {
                 if (first) {
                     fdraw = rnd.Next(1,11);
-                    total = fdraw;
+                    total = total + fdraw;
                     fdraw1 = rnd.Next(1,11);
                     total1 = fdraw1;
                     draw1 = rnd.Next(1,11);
@@ -59,44 +81,92 @@ namespace _21
                 }
                     draw = rnd.Next(1,11);
                     total = total + draw;
-                if (first) {
-                    Console.WriteLine($"Du drog {fdraw} och {draw} för en total: {total}");
-                    Console.WriteLine($"Dator: {total1}");
+
+                if (first) 
+                {
+                    Write($"Du drog {fdraw} och {draw} för en total: {total}");
+                    Write($"Dator: {total1}");
+                    Write("");
                     first = false;
                 }
                 else
                 {
-                    Console.WriteLine($"Du drog {draw} för en total: {total}");   
-                    Console.WriteLine($"Dator: {total1}");
+                    Write("");
+                    Write($"Du drog {draw} för en total: {total}");   
+                    Write($"Dator: {total1}");
+                    Write("");
                 }
-                if (total > 21) {
-                    Console.WriteLine("Datorn vann");
+
+                if (total > 21) 
+                {
+                    Write("Datorn vann");
+                    Write($"Spelare: {total}");
+                    Write($"Dator: {total1}");
+                    Winner = "Datorn";
+                    Write("");
                     break;
                 }
-                Console.WriteLine("Vill du ha ett till kort? (J/N)");
+                Console.Write("Vill du ha ett till kort? (J/N) ");
                 string svar = Console.ReadLine();
                 if (svar.ToLower() == "j") {
                     continue;
                 }
-                else {
-                    while (total1 <= total && total <= 21) 
+                else 
+                {
+                    if (total1 >= total && total1 <= 21 || total > 21) 
+                    {
+                        Write("Datorn vann");
+                        Write($"Spelare: {total}");
+                        Write($"Dator: {total1}");
+                        Write("");
+                        Winner = "Datorn";
+                        break;
+                    }
+
+                    while (total1 <= total && total <= 21 || total1 < 21) 
                     {
                         draw1 = rnd.Next(1,11);
                         total1 = total1 + draw1;
-                        Console.WriteLine($"Dator drog.");
-                        Console.WriteLine($"Dator: {total1}");
+                        Write("");
+                        Write($"Dator drog: {draw1}.");
+                        Write($"Dator: {total1}");
+                        Write("");
+                    }
+                    if (total1 > 21) 
+                    {
+                        Write("Du vann");
+                        Write($"Spelare: {total}");
+                        Write($"Dator: {total1}");
+                        Write("");
+                        win();
+                        break;
                     }
                 }
+
                 if (total1 >= total && total1 <= 21 || total > 21) {
-                    Console.WriteLine("Datorn vann");
+                    Write("Datorn vann");
+                    Write($"Spelare: {total}");
+                    Write($"Dator: {total1}");
+                    Write("");
+                    Winner = "Datorn";
                     break;
                 }
                 if (total > total1 && total <= 21) {
-                     Console.WriteLine("Du vann");
-                     break;
+                    Write("Du vann");
+                    Write($"Spelare: {total}");
+                    Write($"Dator: {total1}");
+                    Write("");
+                    win();
+                    break;
                 }
             }
             Main();
+        }
+
+        static void win() 
+        {
+            Console.Write("Skriv ditt namn: ");
+            Winner = System.Console.ReadLine();
         }
     }
 }

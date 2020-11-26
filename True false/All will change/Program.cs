56af63abeff6673a36a54401936e2cc4e7dd00b0
AlbinWalleniusWoxnerud <albin.walleniuswoxnerud@elev.ga.ntig.se>
2020-11-26 11:04:40 +0100

    5.23
    Array sortering

--- True false/All will change/Program.cs
+++ True false/All will change/Program.cs
@@ -6,21 +6,43 @@ namespace All_will_change
     {
         static void Main()
         {
-            int[] array = { 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 5 };
+            int[] array = { 1, 1, 2, 2, 2, 3, 3, 2, 2 };
             int amountmax = 0;
+            int[] b = new int[array.Length];
+            for (int a = 0; a < b.Length; a++) b[a] = 0;
             for (int i = 0; i < array.Length; i++)
             {
                 int amount = 0;
                 for (int j = 0; j < array.Length; j++) if (array[i] == array[j]) amount++;
                 if (amountmax < amount) amountmax = amount;
             }
+
             for (int i = 0; i < array.Length; i++)
             {
                 int amount = 0;
                 for (int j = 0; j < array.Length; j++) if (array[i] == array[j]) amount++;
-                if (amount == amountmax) Console.WriteLine(array[i]);
+                if (amount == amountmax)
+                {
+                    b[i] = array[i];
+                }
             }
-            
+            for (int i = 0; i < b.Length; i++)
+            {
+                int yes = 0;
+                if (b[i] != 0)
+                {
+                    for (int j = 0; j < b.Length; j++) if (b[i] == b[j])
+                        {
+                            yes++;
+                            if (yes == 1)
+                            {
+                                Console.WriteLine(b[i]);
+                            }
+                        }
+                }
+            }
         }
+
     }
 }
+

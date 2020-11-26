5961b1a728880ca48e56b3045c4b62dd5b037829
AlbinWalleniusWoxnerud <albin.walleniuswoxnerud@elev.ga.ntig.se>
2020-11-26 12:11:56 +0100

    5.23
    Klar, tedde + bool = solution

--- True false/All will change/Program.cs
+++ True false/All will change/Program.cs
@@ -6,10 +6,10 @@ namespace All_will_change
     {
         static void Main()
         {
-            int[] array = { 1, 1, 2, 2, 2, 3, 3, 2, 2 };
+            int[] array = { 1, 1, 3, 3, 7, 4, 3, 6, 2, 2, 2, 3, 3, 2, 2 };
             int amountmax = 0;
             int[] b = new int[array.Length];
-            for (int a = 0; a < b.Length; a++) b[a] = 0;
+            for (int a = 0; a < b.Length; a++) b[a] = 1;
             for (int i = 0; i < array.Length; i++)
             {
                 int amount = 0;
@@ -19,30 +19,24 @@ namespace All_will_change
 
             for (int i = 0; i < array.Length; i++)
             {
+                bool tru = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (array[i] == array[j])
+                    {
+                        tru = true;
+                        break;
+                    }
+                }
+
+                if (tru) continue;
+
                 int amount = 0;
                 for (int j = 0; j < array.Length; j++) if (array[i] == array[j]) amount++;
-                if (amount == amountmax)
-                {
-                    b[i] = array[i];
-                }
+                if (amountmax == amount) Console.WriteLine(array[i]); ;
             }
-            for (int i = 0; i < b.Length; i++)
-            {
-                int yes = 0;
-                if (b[i] != 0)
-                {
-                    for (int j = 0; j < b.Length; j++) if (b[i] == b[j])
-                        {
-                            yes++;
-                            if (yes == 1)
-                            {
-                                Console.WriteLine(b[i]);
-                            }
-                        }
-                }
-            }
+
         }
 
     }
 }
-

91836beb0ee33f92ec2afaf14554139daec82c3a
Albin Wallenius Woxnerud <70270613+AlbinWalleniusWoxnerud@users.noreply.github.com>
2020-11-30 20:23:04 +0100

    kap 7 done
    thank you list very cool

---
+++ Shitshow/Kapitel-7/7.19/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _7._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "hej", "nej", "ja" };
+            Console.WriteLine(cont(array, 'a'));
+        }
+        static bool cont(string[] ar, char a)
+        {
+            int count = 0;
+            for (int i = 0; i < ar.Length; i++)
+            {
+                string temp = ar[i];
+                if (temp.Contains(a))
+                {
+                    count++;
+                }
+            }
+            if (count == ar.Length) return true;
+            else return false;
+        }
+    }
+}
---
+++ Shitshow/Kapitel-7/7.20/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] ar1 = { "a", "b", "b", "c", "hej" };
+            string[] ar2 = { "hej", "a", "a", "b", "programmering" };
+            foreach (string item in both(ar1, ar2))
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static string[] both(string[] a, string[] b)
+        {
+            List<string> nono = new List<string>();
+            for (int i = 0; i < a.Length; i++)
+            {
+                bool tru = false;
+                for (int j = 0; j < i; j++)
+                {
+                    if (a[i] == a[j])
+                    {
+                        tru = true;
+                        break;
+                    }
+                }
+                if (tru) continue;
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if (a[i].Contains(b[j]))
+                    {
+                        nono.Add(b[j]);
+                        break;
+                    }
+                }
+            }
+            return nono.ToArray();
+        }
+    }
+}

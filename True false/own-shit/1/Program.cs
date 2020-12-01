7c6112134b44f146324766ef3d77c5b403ff91ad
Albin Wallenius Woxnerud <70270613+AlbinWalleniusWoxnerud@users.noreply.github.com>
2020-12-01 12:17:01 +0100

    monogame

---
+++ True false/own-shit/1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+    class Car
+    {
+        public string color = "red";
+    }
+}

5d898ca94f61753006509f85926aa90e62eda6be
Albin Wallenius Woxnerud <70270613+AlbinWalleniusWoxnerud@users.noreply.github.com>
2021-01-21 00:00:27 +0100

    Lesson thing

--- Shitshow/Project/Projekt 2/Program.cs
+++ Shitshow/Project/Projekt 2/Program.cs
@@ -10,14 +10,13 @@ namespace Project_2
     {
         static int bordsnr = 8;
         static string[] tempinfo = new string[bordsnr];
-        static int[] guests = new int[bordsnr];
-        static string[] tempguests = new string[bordsnr];
+        static string baseName = "Inga gäster";
         static void slee(string inp)
         {
             for (int i = 0; i <= inp.Length - 1; i++)
             {
                 Console.ForegroundColor = ConsoleColor.White;
-                Thread.Sleep(20);
+                Thread.Sleep(1);
                 Console.Write(inp.Substring(i, 1));
             }
             if (inp.Length <= 10) Thread.Sleep(500);
@@ -32,19 +31,18 @@ namespace Project_2
 
         static void check()
         {
-            if (File.Exists("bord.txt") && File.Exists("guests.txt"))
+            if (File.Exists("bord.txt"))
             {
                 tempinfo = File.ReadAllLines("bord.txt");
-                string[] tempguests = File.ReadAllLines("guests.txt");
-                for (int i = 0; i < tempguests.Length; i++)
-                {
-                    guests[i] = int.Parse(tempguests[i]);
-                }
                 slee("Bordsinformation kan läsas in från fil");
-                slee("Vill du läsa in filen? J/N");
-                string a = Console.ReadLine().ToLower();
-                if (a != "j") { slee("Ny information skapades"); empty(); }
-                else choise();
+                bool choise1 = true;
+                while (choise1)
+                {
+                    slee("Vill du läsa in filen? J/N");
+                    string a = Console.ReadLine().ToLower();
+                    if (a == "n") { slee("Ny information skapades"); choise1 = false; empty(); }
+                    if (a == "j") { choise1 = false; choise(); }
+                }
             }
             else
             {
@@ -54,9 +52,20 @@ namespace Project_2
         }
         static void choise()
         {
-            Console.WriteLine();
-            slee("Välj ett alternativ\n1.Visa alla bord\n2.Lägg till / ändra bordsinformation\n3.Markera att ett bord är tomt\n4.Töm alla bord\n5.Avsluta programmet");
-            int a = int.Parse(Console.ReadLine());
+
+            bool choise3 = false;
+            int a = 0;
+            while (!choise3)
+            {
+                bool choise2 = false;
+                while (!choise2)
+                {
+                    Console.WriteLine();
+                    slee("Välj ett alternativ\n1.Visa alla bord\n2.Lägg till / ändra bordsinformation\n3.Markera att ett bord är tomt\n4.Töm alla bord\n5.Avsluta programmet");
+                    choise2 = int.TryParse(Console.ReadLine(), out a);
+                }
+                if (0 < a && a < 6) choise3 = true;
+            }
             switch (a)
             {
                 case 1:
@@ -81,44 +90,76 @@ namespace Project_2
         {
             Console.WriteLine();
             string[] getbordinfo = File.ReadAllLines("bord.txt");
+            int guest = 0;
             foreach (string bord in getbordinfo)
             {
-                Console.WriteLine(bord);
+                string[] singletableinfo = bord.Split(';');
+                guest += int.Parse(singletableinfo[1]);
+                Console.Write($"Bord {singletableinfo[0]} - antal gäster: {singletableinfo[1]}");
+                if (singletableinfo[2] != baseName)
+                {
+                    Console.Write($" - Namn: { singletableinfo[2]}");
+                }
+                Console.WriteLine();
             }
-            int sum = guests.Sum();
-            Console.WriteLine($"Totalt antal gäster: {sum}");
+            Console.WriteLine($"Totalt antal gäster: {guest}");
             choise();
         }
 
         static void bordinfo()
         {
-            Console.WriteLine();
-            slee("Vilket bordsnummer vill du lägga till/ändra informationen för?");
-            int a = int.Parse(Console.ReadLine());
+
+            bool choise3 = false;
+            bool choise4 = false;
+            int a = 0;
+            while (!choise3)
+            {
+                bool choise2 = false;
+                while (!choise2)
+                {
+                    Console.WriteLine();
+                    slee("Vilket bordsnummer vill du lägga till/ändra informationen för? (1-8)");
+                    choise2 = int.TryParse(Console.ReadLine(), out a);
+                }
+                if (0 < a && a < 9) choise3 = true;
+            }
             a -= 1;
             slee("Skriv in bordets namn");
             string b = Console.ReadLine();
-            slee("Hur många gäster finns vid bordet?");
-            guests[a] = int.Parse(Console.ReadLine());
-            tempguests[a] = ($"{guests[a]}");
-            tempinfo[a] = ($"Bord {a + 1} - Namn: {b}, antal gäster: {guests[a]}");
+
+            int c = 0;
+            while (!choise4)
+            {
+                slee("Hur många gäster finns vid bordet?");
+                choise4 = int.TryParse(Console.ReadLine(), out c);
+            }
+            tempinfo[a] = ($"{a + 1};{c};{b}");
             update();
         }
 
         static void bordtomt()
         {
-            Console.WriteLine();
-            slee("Vilket bordsnummer vill du markera som tomt?");
-            int a = int.Parse(Console.ReadLine());
+            bool choise2 = false;
+
+            int a = 0;
+            while (!choise2)
+            {
+                bool choise1 = false;
+                while (!choise1)
+                {
+                    Console.WriteLine();
+                    slee("Vilket bordsnummer vill du markera som tomt?");
+                    choise1 = int.TryParse(Console.ReadLine(), out a);
+                }
+                if (0 < a && a < 9) choise2 = true;
+            }
             a -= 1;
-            tempinfo[a] = ($"Bord {a + 1} - Inga gäster");
-            guests[a] = 0;
+            tempinfo[a] = ($"{a + 1};0;Inga gäster");
             update();
         }
 
         static void update()
         {
-            File.WriteAllLines("guests.txt", tempguests);
             File.WriteAllLines("bord.txt", tempinfo);
             choise();
         }
@@ -127,13 +168,7 @@ namespace Project_2
         {
             for (int i = 0; i < tempinfo.Length; i++)
             {
-                tempguests[i] = "0";
-            }
-            File.WriteAllLines("guests.txt", tempguests);
-
-            for (int i = 0; i < tempinfo.Length; i++)
-            {
-                tempinfo[i] = ($"Bord {i + 1} - Inga gäster");
+                tempinfo[i] = ($"{i + 1};0;Inga gäster");
             }
             File.WriteAllLines("bord.txt", tempinfo);
             choise();
---
+++ Shitshow/Project/land-csv/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Countries
+{
+    class Program
+    {
+        static void slee(string inp)
+        {
+            for (int i = 0; i <= inp.Length - 1; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Thread.Sleep(1);
+                Console.Write(inp.Substring(i, 1));
+            }
+            if (inp.Length <= 10) Thread.Sleep(500);
+            Console.WriteLine();
+        }
+        static void Main()
+        {
+            Console.WriteLine();
+            slee("Detta är en sökmotor för länder baserade på dess prefix.");
+            Console.WriteLine("Program listar land och landskod.");
+            string[] info = File.ReadAllLines("land.txt");
+
+            bool go = true;
+            while (go)
+            {
+                slee("Ange sökterm (avsluta med return):");
+                string inp = Console.ReadLine().ToLower();
+                if (inp == "") break;
+                foreach (var item in info)
+                {
+                    if (item.Contains(inp))
+                    {
+                        string[] temp = item.Split(',');
+                        Console.WriteLine($"{temp[0]} {temp[1]}: {temp[2]}");
+                    }
+                }
+            }
+        }
+    }
+}

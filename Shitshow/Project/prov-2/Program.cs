92d70185bf78c3fd8dde7da94cfdaad19a73dd61
AlbinWalleniusWoxnerud <albin.walleniuswoxnerud@elev.ga.ntig.se>
2021-03-25 11:00:39 +0100

    Prov 3
    <>

---
+++ Shitshow/Project/prov-2/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace prov_2
+{
+    class Program
+    {
+        static string[] filmer = File.ReadAllLines("movies.tsv");
+        static List<string> fName = new List<string>();
+        static List<string> fMoney = new List<string>();
+        static List<string> fYear = new List<string>();
+        static void Main()
+        {
+            Console.WriteLine("Programmet för att söka filmer.");
+            foreach (string item in filmer)
+            {
+                string[] iFilm = item.Split("\t");
+                fName.Add($"{iFilm[0]}");
+                fMoney.Add($"{iFilm[1]}");
+                fYear.Add($"{iFilm[2]}");
+            }
+            bool runT = true;
+            while (runT)
+            {
+                bool correktInp = false;
+                bool correktInp2 = false;
+                int val = 0;
+                while (!correktInp)
+                {
+                    correktInp2 = false;
+                    while (!correktInp2)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("1.Visa alla filmer\n2.Sök år\n3.Avsluta\nVälj ett alternativ: ");
+                        correktInp2 = int.TryParse(Console.ReadLine(), out val);
+                    }
+                    if (0 < val && val < 4)
+                    {
+                        correktInp = true;
+                    }
+                }
+                switch (val)
+                {
+                    case 1:
+                        Console.WriteLine($"Hittade {fName.Count} filmer:");
+                        for (int i = 0; i < fName.Count; i++)
+                        {
+                            Console.WriteLine($"{i + 1}: {fName[i]} från {fYear[i]}");
+                        }
+                        break;
+                    case 2:
+
+                        bool correktYear = false;
+                        int yearVal = 0;
+                        while (!correktYear)
+                        {
+                            bool correktYearInp = false;
+                            while (!correktYearInp)
+                            {
+                                Console.WriteLine("Vilket år söker du? ");
+                                correktYearInp = int.TryParse(Console.ReadLine(), out yearVal);
+                            }
+                            if (1990 < yearVal && yearVal < 2022) correktYear = true;
+                            else Console.WriteLine("Icke giltigt år, vg försök igen");
+                        }
+                        List<string> fYearIndex = new List<string>();
+
+                        int count = 0;
+                        string summa = "";
+                        string retard = "";
+                        int summaT = 0;
+                        foreach (var item in filmer)
+                        {
+                            if (item.Contains($"{yearVal}"))
+                            {
+                                count++;
+                                string[] temp = item.Split('\t');
+                                for (int i = 1; i < temp[1].Length - 1; i++)
+                                {
+                                    if ((temp[1].Substring(i, 1) != ",")) summa += int.Parse((temp[1].Substring(i, 1)));
+                                }
+                                summaT += int.Parse(summa);
+                                summa = "";
+                            }
+                        }
+
+                        retard += summaT;
+                        for (int i = 0; i < retard.Length; i++)
+                        {
+
+                        }
+
+                        if (count > 0)
+                        {
+                            int count2 = 1;
+                            Console.WriteLine();
+                            Console.WriteLine($"Hittade {count} filmer från {yearVal}");
+                            Console.WriteLine($"Total summa intäkter ${summaT}");
+                            foreach (var item in filmer)
+                            {
+
+                                if (item.Contains($"{yearVal}"))
+                                {
+                                    string[] temp = item.Split('\t');
+                                    Console.WriteLine();
+                                    Console.WriteLine($"Summa intäkter: {temp[1]}");
+                                    Console.WriteLine($"{count2}: {temp[0]}");
+                                    count2++;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Hittade inga filmer");
+                        }
+                        break;
+                    case 3:
+                        runT = false;
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Shitshow/Project/prov3/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace prov3
+{
+    class Program
+    {
+        /// <summary>
+        /// Kod för ålder på vald planet
+        /// </summary>
+        static void Main()
+        ///Main, loopar genom metoder och kallar dem
+        {
+            Console.WriteLine("Program som räknar ut din ålder på en annan planet.");
+            double choise = Age();
+            while (true)
+            {
+                string ageOnChoisePlanet = writeMenu(choise);
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(ageOnChoisePlanet);
+                Console.ResetColor();
+            }
+        }
+        static double Age()
+        ///Return Age
+        {
+            ///Kollar att åldern är ett nummer samt att det är innanför spanet av 1-129 år
+            ///Om man väljer något annat än 1-129 får man göra om
+            Console.WriteLine("Ange din ålder i jordår: ");
+            int temp = 0;
+            bool correkt = false;
+            while (!correkt)
+            {
+                bool trueCorrekt = false;
+                while (!trueCorrekt)
+                {
+                    trueCorrekt = int.TryParse(Console.ReadLine(), out temp);
+                    if (!trueCorrekt)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Wrong input, try again!");
+                        Console.ResetColor();
+                        Console.WriteLine("Ange din ålder i jordår: ");
+                    }
+                }
+                if (0 < temp && temp < 130) correkt = true;
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Wrong input, try again!");
+                    Console.ResetColor();
+                    Console.WriteLine("Ange din ålder i jordår: ");
+                }
+            }
+            return temp;
+        }
+        static string writeMenu(double age)
+        ///Skriv ut alla alternativ samt return string hur gammal man är på vald planet
+        {
+            string alternativ = ("\nVälj ett alternativ\n1.Räkna ut ålder på Merkurius\n2.Räkna ut ålder på Venus\n3.Räkna ut ålder på Mars\n4.Räkna ut ålder på Jupiter\n5.Räkna ut ålder på Saturnus\n6.Räkna ut ålder på Uranus\n7.Räkna ut ålder på Neptunus\n8.Avsluta");
+            Console.WriteLine(alternativ);
+            int temp = 0;
+
+            ///Kollar att du har vald ett av de alternativ som finns att välja mellan, dvs 1-8
+            ///Om man väljer något annat än 1-8 får man göra om
+            bool correkt = false;
+            while (!correkt)
+            {
+                bool trueCorrekt = false;
+                while (!trueCorrekt)
+                {
+                    trueCorrekt = int.TryParse(Console.ReadLine(), out temp);
+                    if (!trueCorrekt)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Wrong input, try again!");
+                        Console.ResetColor();
+                        Console.WriteLine(alternativ);
+                    }
+                }
+                if (0 < temp && temp < 9) correkt = true;
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Wrong input, try again!");
+                    Console.ResetColor();
+                    Console.WriteLine(alternativ);
+                }
+            }
+            ///Beronde på vald plnaet return olika åldrar
+            switch (temp)
+            {
+                case 1:
+                    return ($"På Merkurius är du {age / 0.2408467} år gammal.");
+                case 2:
+                    return ($"På Venus är du {age / 0.61519726} år gammal.");
+                case 3:
+                    return ($"På Mars är du {age / 1.8808158} år gammal.");
+                case 4:
+                    return ($"På Jupiter är du {age / 11.862615} år gammal.");
+                case 5:
+                    return ($"På Saturnus är du {age / 29.447498} år gammal.");
+                case 6:
+                    return ($"På Uranus är du {age / 84.016846} år gammal.");
+                case 7:
+                    return ($"På Neptunus är du {age / 164.79132} år gammal.");
+                case 8:
+                    Console.WriteLine($"Avslutar");
+                    System.Environment.Exit(0);
+                    return "Exit";
+            }
+            ///Blir arg annars, all paths do not return a value.
+            return "Exit";
+        }
+
+    }
+}

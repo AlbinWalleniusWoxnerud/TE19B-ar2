c2454afe57386963002298234dade551fd518c3e
Albin Wallenius Woxnerud <70270613+AlbinWalleniusWoxnerud@users.noreply.github.com>
2021-05-27 23:12:44 +0200

    .

---
+++ True false/own-shit/thingThingthing/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace thingThingthing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.X = 7;
+
+            Point p2 = p1;             // Copies p1 reference
+
+            Console.WriteLine(p1.X);  // 7
+            Console.WriteLine(p2.X);  // 7
+
+            p1.X = 9;                  // Change p1.X
+
+            Console.WriteLine(p1.X);  // 9
+            Console.WriteLine(p2.X);  // 9
+        }
+    }
+    public class UnitConverter
+    {
+        int ratio;
+        public UnitConverter(int unitRatio)
+        {
+            ratio = unitRatio;
+        }
+        public int Convert(int unit)
+        {
+            return unit * ratio;
+        }
+    }
+    public struct Point { public int X, Y; }
+}
